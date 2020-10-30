d6760b4b3f49b3251c9f0b5a5025a0dd28fd74fb
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-10-30 10:51:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,8 @@
     </head>
     <body>
         <h1>My Website</h1>
-        <p>Fullname: Yutthasil Butyojunto
-            Nickname: Vee
-        </p>
-
+        <p>Fullname: Yutthasil Butyojunto</p>
+        <p>Nickname: Vee</p>
         <img src="cat.jpg">
         <br>
         <h2>Short Story</h2>
