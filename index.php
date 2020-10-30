1518f56eebd9993625f893e711dc4d5bad954e50
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-10-30 11:03:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <html>
     <head>
         <title>ITF-Lab1</title>
+        <meta charset="UTF-8">
     </head>
     <body>
         <h1>My Website</h1>
         <p>Fullname: Yutthasil Butyojunto</p>
         <p>Nickname: Vee</p>
         <img src="dog.jpg">
+        <br>
+        <img src="cat.jpg">
         <br>
         <h2>Short Story</h2>
         <p>paragraph 2</p>
