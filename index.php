6a6e1057f1e31993aae62a59d9aa3692f23a907b
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-10-30 10:58:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
         <h1>My Website</h1>
         <p>Fullname: Yutthasil Butyojunto</p>
         <p>Nickname: Vee</p>
-        <img src="cat.jpg">
+        <img src="dog.jpg">
         <br>
         <h2>Short Story</h2>
         <p>paragraph 2</p>
-        <a href="http://www.it.kmitl.ac.th">My Favorite Website</a>
+        <a href="https://www.youtube.com/">My Favorite Website</a>
 
     </body>
 </html>
