04599b63e038c62345bfe874673dfad0b3cf7e60
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-10-30 10:48:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
-echo "<h2>PHP is FUn!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+    <head>
+        <title>ITF-Lab1</title>
+    </head>
+    <body>
+        <h1>My Website</h1>
+        <p>Fullname: Yutthasil Butyojunto
+            Nickname: Vee
+        </p>
+
+        <img src="cat.jpg">
+        <br>
+        <h2>Short Story</h2>
+        <p>paragraph 2</p>
+        <img src="">
+        <a href="http://www.it.kmitl.ac.th">My Favorite Website</a>
+
+    </body>
+</html>
