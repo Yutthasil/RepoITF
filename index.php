7bf21c67ef25aeb3110ddaaafdb7b691cb6fac53
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-11-06 09:33:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,25 +2,28 @@
     <head>
         <title>ITF-Lab1</title>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
     </head>
     <body>
-        <h1>My Website</h1>
-        <hr>
-        <p>Fullname: Yutthasil Butyojunto</p>
-        <p>Nickname: Vee</p>
-        <a href="https://www.w3schools.com/">My Favorite Website</a>
-        <p></p>
-        <img src="dog.jpg" width="350" height="287">
-        <br>
-        <img src="blobfish.jpg" width="200" height="200">
-        <br>
-        <h2>Short Gag</h2>
-        <hr>
-        <h3>ประจำเดือนไม่มา</h3>
-        <p>Q: กินยาคุมฉุกเฉินไปครั้งที่สองประมาณอาทิตย์กว่าเเล้วประจำเดือนยังไม่มา</p>
-        <p>   พอไปซื้อที่ตรวจมาตรวจแล้วไม่ท้องเป็นเพราะประจำเดือนเลื่อนหรือป่าวคะ</p>
-        <p>   ช่วยตอบหน่อย</p>
-        <p>A: ไม่ท้องก็ดีแล้วครับ แต่ถ้าถามว่า ประจำเดือนเลื่อนมั้ย ถ้าใส่เบรคมือไว้ หรือ</p>
-        <p>   เข้าเกียร์ P ก็ไม่น่าจะเลื่อนนะครับ</p>
+        <div class="container">
+            <h1>My Website</h1>
+            <hr>
+            <p>Fullname: Yutthasil Butyojunto</p>
+            <p>Nickname: Vee</p>
+            <a href="https://www.w3schools.com/">My Favorite Website</a>
+            <p></p>
+            <img src="dog.jpg" width="350" height="287">
+            <br>
+            <img src="blobfish.jpg" width="200" height="200">
+            <br>
+            <h2>Short Gag</h2>
+            <hr>
+            <h3>ประจำเดือนไม่มา</h3>
+            <p>Q: กินยาคุมฉุกเฉินไปครั้งที่สองประมาณอาทิตย์กว่าเเล้วประจำเดือนยังไม่มา</p>
+            <p>   พอไปซื้อที่ตรวจมาตรวจแล้วไม่ท้องเป็นเพราะประจำเดือนเลื่อนหรือป่าวคะ</p>
+            <p>   ช่วยตอบหน่อย</p>
+            <p>A: ไม่ท้องก็ดีแล้วครับ แต่ถ้าถามว่า ประจำเดือนเลื่อนมั้ย ถ้าใส่เบรคมือไว้ หรือ</p>
+            <p>   เข้าเกียร์ P ก็ไม่น่าจะเลื่อนนะครับ</p>
+            <div/>
     </body>
 </html>
