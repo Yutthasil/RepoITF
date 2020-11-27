e7134704a7ab5eb4ea5ada9015a512ce8dab981f
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-11-27 09:28:02 +0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,17 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itflabdatabase.mysql.database.azure.com', 'Yutthasil@itflabdatabase', 'KDByeq34', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$id = $_GET['id'];
+$sql = "DELETE FROM guestbook WHERE id = $id";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  mysqli_close($conn);
+?>
