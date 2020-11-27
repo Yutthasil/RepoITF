5d361daa66026fba1c1fb70e01dd723df5aa9fc1
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-11-27 09:41:22 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itflabdatabase.mysql.database.azure.com', 'Yutthasil@itflabdatabase', 'KDByeq34', 'itflab', 3306);
+mysqli_real_connect($conn, 'itflabdatabase.mysql.database.azure.com', 'Yutthasil@itflabdatabase', 'KDByeq34', 'Labfinal', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
