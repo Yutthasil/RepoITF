bff4f914b595dd24721da96a8f669979fbbef377
Yutthasil <73679723+Yutthasil@users.noreply.github.com>
2020-11-27 09:41:24 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,19 +1,19 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itflabdatabase.mysql.database.azure.com', 'Yutthasil@itflabdatabase', 'KDByeq34', 'itflab', 3306);
+mysqli_real_connect($conn, 'itflabdatabase.mysql.database.azure.com', 'Yutthasil@itflabdatabase', 'KDByeq34', 'Labfinal', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
+$product = $_POST['product'];
+$price = $_POST['price'];
+$amount = $_POST['amount'];
 
 
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+$sql = "INSERT INTO guestbook (product , price , amount, total) VALUES ('$product', '$price', '$amount', '$price*$amount')";
 
 
 if (mysqli_query($conn, $sql)) {
